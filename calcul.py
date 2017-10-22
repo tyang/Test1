92fa138164bf1c17cda13dbb320e28502f682838
Tong <tong.yang@outlook.fr>
2017-10-22 22:42:08 +0200

    change

--- calcul.py
+++ calcul.py
@@ -1,7 +1,9 @@
+'''
+coucou
+'''
 
 res = 0
-
-for i in range(1,3000000000000000000000000):
+for i in range(1, 3000):
     prev_res = res
     res = res+i
-    print('{ 0 } + { 1 } = { 2 }'.format(prev_res,i,res))
+    print('{0} + {1} = {2}'.format(prev_res, i, res))
