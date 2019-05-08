a1fc1a95bd11a68f0ccacdb9e1bfbd6792149d91
Tong <tong.yang@outlook.fr>
2019-05-08 23:46:55 +0200

    commit

--- calcul.py
+++ calcul.py
@@ -1,6 +1,7 @@
 '''
 coucou c nous
 '''
+import request
 
 res = 0
 for i in range(1, 3000):
@@ -8,5 +9,4 @@ for i in range(1, 3000):
     res = res+i
     print('{0} + {1} = {2}'.format(prev_res, i, res))
 
-print("coucou cest nous")
-
+print("coucou cest nous 2")
